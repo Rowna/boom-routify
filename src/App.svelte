bd6b79cf0287c5f0950a61bf5811896cdb0efede
Rowan Kh <rowan.kh1@gmail.com>
2022-04-11 16:56:01 +0200

    w: Firebase init

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,57 @@
 <script>
+  import firebase_config from "./server/firebase_config";
+
+  // Firebase 9
+  import { initializeApp } from "firebase/app";
+  // firestore-hook installieren
+  import { 
+    getFirestore, 
+    collection,
+    getDocs
+   } from "firebase/firestore";
+
+
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
+
+
+  // Firebase 9
+  const fb = initializeApp(firebase_config);
+
+  // firestore-Connector initialisiern
+  // damit haben wir die Verbindung zu meiner Firestore-Datenbank
+  // im Firebase-"svelte-bulma"-Projekt.
+  const db = getFirestore();
+
+
+  // Connector zur "articles"-Collecion erstellen
+  const fbArticles = collection(db, "articles");
+
+  // Artikel-"Zip"aus Firestore downloaden;
+  // der Snapshot ist wie eine Zip-Datei, die ich erst entpacken muss,
+  // um weite mit den Daten arbeiten zu koennen.
+  // Diese Aktion ist ASYNCHRON!, deshalb muss ich mit Promises weiterarbeiten
+  getDocs(fbArticles)
+    .then((snapshot) => {
+      // console.table(snapshot.docs);
+      let theArticles = []
+      snapshot.docs.forEach((doc) => {
+            // die Daten aud dem Firebase-"document" , die drin stehen.
+        theArticles.push({ id: doc.id, ...doc.data()})
+      })
+      // console.log(theArticles);
+  })
+  .catch(error => console.error(error));
+
 </script>
 
-<Router {routes} />
+<Router {routes} /> 
+
+<!--  -->
+<style global lang="scss">
+  @import "./main.scss";
+  .block {
+    padding: 2rem 6rem;
+  }
+
+</style>
---
+++ src/server/firebase_config.js
@@ -0,0 +1,12 @@
+// src/firebase_config.ts
+export default {
+    // meine firebase config object here
+    apiKey: "AIzaSyC79rGpPx7kU4aPS_0FD9JKamfIDViJMds",
+    authDomain: "svelte-bulma.firebaseapp.com",
+    databaseURL: "https://svelte-bulma-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "svelte-bulma",
+    storageBucket: "svelte-bulma.appspot.com",
+    messagingSenderId: "511320383870",
+    appId: "1:511320383870:web:a57a73f5342625ebe7532f",
+    measurementId: "G-G0MD4X123G"
+};
