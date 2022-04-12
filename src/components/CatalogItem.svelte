b35a5c5ebd17fc8572ef6b89f9fad1d4d001c726
Rowan Kh <rowan.kh1@gmail.com>
2022-04-12 11:41:57 +0200

    w: Catalog-Seite erstellt

---
+++ src/components/CatalogItem.svelte
@@ -0,0 +1,67 @@
+<script>
+  export let article = {};
+  // const myArtikels = [];
+</script>
+
+<!--
+<div class="catalog-container card">
+    <p><img src="images/{article.img}" alt="Bild">{article.img}</p>
+    <p>My title: {article.title}</p>
+    <p>My price: {article.price}</p>
+    <p>My description: {article.desc}</p>
+</div>
+-->
+<div class="catalog-item card">
+  <div class="card-image">
+    <figure class="image">
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img class="img" src="images/{article.img}" alt="article image" />
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media-content">
+      <p class="title is-4">{article.title}</p>
+    </div>
+
+    <div class="content">
+      {article.desc}
+    </div>
+  </div>
+
+  <footer class="card-footer">
+      <p class="card-footer-item">
+          <span class="cart-img">
+            <img src="../public/images/shopping-cart.png" alt="cart">
+          </span>
+      </p>
+      <p class="card-footer-item">
+          <span class="fav-img">
+            <img src="../public/images/herz-ohne.png" alt="cart">
+          </span>
+      </p>
+  </footer>
+</div>
+
+<style>
+  .catalog-item {
+    max-width: 500px;
+  }
+  .image {
+    max-height: 770px;
+    max-width: 600px;
+  }
+  .img {
+      height: 450px;
+      width: 450px;
+  }
+  .card-footer-item:hover {
+      cursor: pointer;
+      background-color: #f6919e;
+  }
+  .cart-img, .fav-img {
+      max-width: 40px;
+  }
+  .cart-img, .fav-img:hover {
+      cursor: pointer;
+  }
+</style>
--- src/pages/catalog.svelte
+++ src/pages/catalog.svelte
@@ -1,12 +1,76 @@
 <script>
+  import CatalogItem from "../components/CatalogItem.svelte";
+
+  // firestore-hook installieren
+  import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+  let docs = [];
+
+  // firestore-Connector initialisiern
+  // damit haben wir die Verbindung zu meiner Firestore-Datenbank
+  // im Firebase-"svelte-bulma"-Projekt.
+  const db = getFirestore();
+
+  // Connector zur "articles"-Collecion erstellen
+  const fbArticles = collection(db, "articles");
+
+  // Artikel-"Zip"aus Firestore downloaden;
+  // der Snapshot ist wie eine Zip-Datei, die ich erst entpacken muss,
+  // um weite mit den Daten arbeiten zu koennen.
+  // Diese Aktion ist ASYNCHRON!, deshalb muss ich mit Promises weiterarbeiten
+  getDocs(fbArticles)
+    .then((snapshot) => {
+      console.log("bin im Promise!");
+      let theArticles = [];
+      snapshot.docs.forEach((doc) => {
+        // die Daten aud dem Firebase-"document" , die drin stehen.
+        theArticles.push({ id: doc.id, ...doc.data() });
+      });
+      docs = theArticles;
+      // console.log(theArticles);
+    })
+    .catch((error) => console.error(error));
 </script>
 
-<h1>Hier kommt der Katalog hin!</h1>
-
-<p>
-  Aber dass man ihn so einfach aufrufen kann, das liegt an der Genialitaet von
-  Routify.
-</p>
+<div class="catalog">
+  <div class="catalog-title">
+    <p>BOOM</p>
+  </div>
+  <div class="catalog-container">
+    {#each docs as article (article.id)}
+      <CatalogItem {article} />
+    {/each}
+  </div>
+</div>
 
 <style>
+  .catalog {
+    margin: 8rem 10rem;
+  }
+  .catalog-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-items: center;
+    row-gap: 30px;
+    column-gap: 5px;
+  }
+  @media only screen and (max-width: 950px) {
+    .catalog-container {
+      flex-direction: column;
+      row-gap: 10px;
+      column-gap: 10px;
+    }
+  }
+  @media only screen and (max-width: 1950px) {
+    .catalog-container {
+      flex-direction: column;
+      row-gap: 30px;
+      column-gap: 30px;
+    }
+  }
+  @media only screen and (max-width: 800px) {
+    .catalog {
+      margin: 15px 15px;
+    }
+  }
 </style>
